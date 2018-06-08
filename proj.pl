0574e350ee6b41efeb375289ab2c41ac9401e539
CarolineNakazato <carolinenakazato@yahoo.com.br>
2018-06-08 14:00:55 -0300

    Add files via upload

--- proj.pl
+++ proj.pl
@@ -2,21 +2,15 @@
 %acesso(campinas,"Sao Paulo",bandeirantes,100,(1:00)).
 %acesso(campinas,"Sao Paulo",anhaguera,90,(0:50)).
 %acesso(sorocaba,campinas,"Santos Dumont",110,(1:10)).
-acesso(a, b, ab, 5, (0:21)).
-acesso(a, b, ab2, 10, (0:15)).
-acesso(b, c, bc, 8, (0:19)).
-acesso(b, d, bd, 14, (0:13)).
-acesso(d, e, de, 13, (0:23)).
-acesso(d, e, de2, 609, (2:46)).
-acesso(a, f, af, 8, (0:24)).
-acesso(a, f, af2, 6, (0:36)).
-acesso(a, g, ag, 100, (2:15)).
-acesso(a, g, ag2, 90, (2:58)).
-acesso(g, h, gh, 50, (0:49)).
-
+acesso(a, b, x1, 100, (1:10)).
+acesso(a, b, x2, 110, (1:00)).
+acesso(b, c, x3, 100, (2:10)).
+acesso(c,d, x4, 100, (00:30)).
+acesso(a, d, x5, 300, (3:30)).
+acesso(a, e, x6, 100, (1:10)).
+acesso(e, c, x7, 250, (2:40)).
 %regras
 rotaDireta(CidadeA,CidadeB,Rodovia):-acesso(CidadeA,CidadeB,Rodovia,_,_).
-rotaDireta(CidadeA,CidadeB,Rodovia):-acesso(CidadeB,CidadeA,Rodovia,_,_).
 
 pertence(X,[X|_]) :- !.
 pertence(X,[_|L]) :- pertence(X,L).
@@ -29,12 +23,8 @@ add(X,[Y|L1],[Y|L2]):-
 caminho(CidadeA,CidadeB,Rodovias):-rotaDireta(CidadeA,CidadeB,Rodovia),
     add(Rodovia,[],Rodovias).
 caminho(CidadeA,CidadeB,Rodovias):-acesso(CidadeC,CidadeB,Rodovia,_,_),
-    caminho(CidadeC,CidadeC,Lista),
+    caminho(CidadeA,CidadeC,Lista),
     add(Rodovia,Lista,Rodovias).
-caminho(CidadeA,CidadeB,Rodovias):-acesso(CidadeC,CidadeA,Rodovia,_,_),
-    caminho(CidadeB,CidadeC,Lista),
-    add(Rodovia,Lista,Rodovias).
-
 
 menorLista(X,[X]).
 menorLista(N,[X,Y|Cauda]):-
